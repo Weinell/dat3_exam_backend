93ab65828159b629da31a42cab253b7542ce8cee
Nikolaj <nikolajweinell@hotmail.com>
2022-06-15 11:08:35 +0200

    ApplicationConfig works? Test on droplet

--- src/main/java/rest/MatchResource.java
+++ src/main/java/rest/MatchResource.java
@@ -1,5 +1,11 @@
 package rest;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import facades.MatchFacade;
+import utils.EMF_Creator;
+
+import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -7,6 +13,10 @@ import javax.ws.rs.core.MediaType;
 
 @Path("match")
 public class MatchResource {
+
+    private static final EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
+    private static final MatchFacade facade = MatchFacade.getFacade(emf);
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     @Path("alive")
     @GET
