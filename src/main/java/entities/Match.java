ba50cdb50cbe1bc8f58d1c1db34e61bb71bbfd80
Nikolaj <nikolajweinell@hotmail.com>
2022-06-15 08:17:26 +0200

    Setup  attributes

--- src/main/java/entities/Match.java
+++ src/main/java/entities/Match.java
@@ -1,17 +1,30 @@
 package entities;
 
+import org.graalvm.compiler.graph.Node;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "matches")
 public class Match {
     @Id
-    @Column(name = "id", nullable = false)
+    @Column(name = "id")
+    @NotNull
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "opponentTeam")
+    private String opponentTeam;
 
+    @Column(name = "judge")
+    private String judge;
 
+    @Column(name = "type")
+    private int type;
+
+    @Column(name = "inDoors")
+    private boolean inDoors;
 
 
     public Long getId() {
